fd53eda4878f0c08730bab13f6ae6e448042bb3a
mauro <freeMonkey-ls@protonmail.com>
2022-04-19 22:06:58 +0200

    REFACTORING: estratto metodo partizionamento e scambio da sort

--- src/main/java/QuickMergeSort.java
+++ src/main/java/QuickMergeSort.java
@@ -5,6 +5,11 @@ public class QuickMergeSort {
     }
 
     public int[] sort() {
+        partition();
+        return arraytoSort;
+    }
+
+    private void partition() {
         if ( (arraytoSort!=null) && (arraytoSort.length>1) ) {
             int pointerLeft=0;
             int pivot=arraytoSort[pointerLeft];
@@ -20,17 +25,19 @@ public class QuickMergeSort {
                 } while ( (pointerLeft<arraytoSort.length) && (arraytoSort[pointerLeft] <= pivot) );
                 /* se non ho controllato tutti gli elementi, scambia quelli puntati dai due puntatori */
                 if (pointerLeft < pointerRight) {
-                    int temp = arraytoSort[pointerLeft];
-                    arraytoSort[pointerLeft] = arraytoSort[pointerRight];
-                    arraytoSort[pointerRight] = temp;
+                    swap(pointerLeft, pointerRight);
                 }
             }
             /* l'ultimo elemento puntato dal puntatore destro sarà l'ultimo inferiore o uguale al perno.
             Lo scambio con questo in modo da avere un array con tutti gli elemnti inferiori o uguali al perno a sinistra di questo e tutti quelli maggiori alla sua destra */
-            arraytoSort[0] = arraytoSort[pointerRight];
-            arraytoSort[pointerRight] = pivot;
+            swap(0,pointerRight);
         }
-        return arraytoSort;
+    }
+
+    private void swap(int pointer1, int pointer2) {
+        int temp = arraytoSort[pointer1];
+        arraytoSort[pointer1] = arraytoSort[pointer2];
+        arraytoSort[pointer2] = temp;
     }
 
     public void setArrayToSort(int[] newArrayToSort) {
