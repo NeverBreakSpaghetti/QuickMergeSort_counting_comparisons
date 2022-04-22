3105e346ecf1ee47bd9a451e260ae0c23c791bab
mauro <freeMonkey-ls@protonmail.com>
2022-04-22 19:26:45 +0200

    VERDE: numero delle chiamate mergesort ricorsive di firstStep test passato

--- src/test/java/QuickMergeSortTest.java
+++ src/test/java/QuickMergeSortTest.java
@@ -214,4 +214,15 @@ public class QuickMergeSortTest {
         mockQuickMergeSort.firstStep(2,3,0);
         verify(mockQuickMergeSort,never()).mergeSort(anyInt(),anyInt(),anyInt());
     }
+
+    @Test
+    public void firstStepMergeSortNumberOfRecursionTest(){
+        doCallRealMethod().when(mockQuickMergeSort).setArrayToSort(any());
+        doCallRealMethod().when(mockQuickMergeSort).mergeSort(anyInt(),anyInt(),anyInt());
+        doCallRealMethod().when(mockQuickMergeSort).firstStep(anyInt(),anyInt(),anyInt());
+
+        mockQuickMergeSort.setArrayToSort(new int[]{0,1,2,3,4,5,6,7,8});
+        mockQuickMergeSort.firstStep(5,9,0);
+        verify(mockQuickMergeSort, times(6)).mergeSort(anyInt(),anyInt(),anyInt());
+    }
 }
