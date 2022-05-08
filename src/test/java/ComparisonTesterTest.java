ad3752923497d191db1f9c5342ed21561a2a163b
mauro <freeMonkey-ls@protonmail.com>
2022-05-08 16:54:11 +0200

    ROSSO: implementare lancio eccezione IllegalArgumentException in caso di setter e inizializzazione oggetto comparisonTester con numero di elementi dell'array negativo

--- src/test/java/ComparisonTesterTest.java
+++ src/test/java/ComparisonTesterTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ComparisonTesterTest {
 
@@ -20,5 +21,11 @@ public class ComparisonTesterTest {
     public void comparisonsTwoElementsArraySortTest(){
         comparisonTester.setNElements(2);
         assertThat(comparisonTester.countComparisonsSort()).isEqualTo(3);
+    }
+
+    @Test
+    public void comparisonsNegativeNumberElementsArraySortTest(){
+        assertThrows(IllegalArgumentException.class, () -> new ComparisonTester(-3));
+        assertThrows(IllegalArgumentException.class, () -> comparisonTester.setNElements(-5));
     }
 }
