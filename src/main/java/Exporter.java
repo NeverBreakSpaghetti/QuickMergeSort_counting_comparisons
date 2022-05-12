fd1a345ea920ef6ea5856276832cf85bdb62d6f6
mauro <freeMonkey-ls@protonmail.com>
2022-05-12 16:58:08 +0200

    VERDE: conversione nel formato csv di un array vuoto implementata

--- src/main/java/Exporter.java
+++ src/main/java/Exporter.java
@@ -1,6 +1,11 @@
 public class Exporter {
 
     public String convertToCSVRow(String stringToConvert) {
-        return null;
+        String resultRow = stringToConvert;
+        if ( resultRow != null && ( resultRow.contains("[") || resultRow.contains("]") ) ) {
+            resultRow=resultRow.replaceAll("\\[", "");
+            resultRow=resultRow.replaceAll("\\]", "");
+        }
+        return resultRow;
     }
 }
