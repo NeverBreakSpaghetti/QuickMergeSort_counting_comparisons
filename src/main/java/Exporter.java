133130cc771bda2b22cf62c17b830bbb518315eb
mauro <freeMonkey-ls@protonmail.com>
2022-05-12 18:33:38 +0200

    REFACTORING: estratta costante per carattere separatore

--- src/main/java/Exporter.java
+++ src/main/java/Exporter.java
@@ -1,11 +1,13 @@
 public class Exporter {
 
+    public static final String SEPARATOR = ";";
+
     public String convertToCSVRow(String stringToConvert) {
         String resultRow = stringToConvert;
         if ( resultRow != null && ( resultRow.contains("[") || resultRow.contains("]") || resultRow.contains(",") ) ) {
             resultRow=resultRow.replaceAll("\\[", "");
             resultRow=resultRow.replaceAll("\\]", "");
-            resultRow=resultRow.replaceAll(",",";");
+            resultRow=resultRow.replaceAll(",", SEPARATOR);
             resultRow=resultRow.replaceAll(" ","");
         }
         return resultRow;
