f3c2fde126ef4fb1812c7857f93b67546d73c76c
mauro <freeMonkey-ls@protonmail.com>
2022-05-12 18:27:38 +0200

    VERDE: conversione nel formato csv di un array da due elementi implementato

--- src/main/java/Exporter.java
+++ src/main/java/Exporter.java
@@ -2,9 +2,11 @@ public class Exporter {
 
     public String convertToCSVRow(String stringToConvert) {
         String resultRow = stringToConvert;
-        if ( resultRow != null && ( resultRow.contains("[") || resultRow.contains("]") ) ) {
+        if ( resultRow != null && ( resultRow.contains("[") || resultRow.contains("]") || resultRow.contains(",") ) ) {
             resultRow=resultRow.replaceAll("\\[", "");
             resultRow=resultRow.replaceAll("\\]", "");
+            resultRow=resultRow.replaceAll(",",";");
+            resultRow=resultRow.replaceAll(" ","");
         }
         return resultRow;
     }
