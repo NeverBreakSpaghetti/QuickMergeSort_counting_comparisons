12993fbd0db44dbf05d4faebfb82f30eebb15ce9
mauro <freeMonkey-ls@protonmail.com>
2022-05-13 12:20:37 +0200

    ROSSO: implementare scrittura del file csv

--- src/test/java/ExporterTest.java
+++ src/test/java/ExporterTest.java
@@ -1,6 +1,8 @@
+import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -101,5 +103,33 @@ public class ExporterTest {
 
         exporter.setSeparator(";");
         assertThat(exporter.convertComparisonsBySortingAlgorithm(sortingAlgorithmList,comparisonsArray)).isEqualTo("\"QuickMergeSort\";3;4;9");
+    }@Test
+    public void createNotEmptyCSVTest() {
+        Exporter exporter = new Exporter();
+        String fileName = "test3.csv";
+        List<SorterState> sortingAlgorithmList = new ArrayList<>();
+        sortingAlgorithmList.add(new QuickMergeSort());
+        sortingAlgorithmList.add(new QuickSort());
+        int[][] dummyComparisonsNumberArray = new int[][]{{2,6},{8,1}};
+
+        exporter.export(fileName,sortingAlgorithmList,dummyComparisonsNumberArray);
+
+        List<List<String>> records = new ArrayList<List<String>>();
+        try {
+            CSVReader csvReader = new CSVReader(new FileReader(fileName));
+            String[] values = null;
+            while ((values = csvReader.readNext()) != null)
+                records.add(Arrays.asList(values));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        assertThat(records.get(0).get(0)).isEqualTo("QuickMergeSort");
+        assertThat(records.get(0).get(1)).isEqualTo("2");
+        assertThat(records.get(0).get(2)).isEqualTo("6");
+        assertThat(records.get(1).get(0)).isEqualTo("QuickSort");
+        assertThat(records.get(1).get(1)).isEqualTo("8");
+        assertThat(records.get(1).get(2)).isEqualTo("1");
+        new File(fileName).delete();
     }
 }
