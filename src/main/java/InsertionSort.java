94f1cc7ef5061a66fed129850f610e91cfb90996
mauro <freeMonkey-ls@protonmail.com>
2022-05-13 17:29:25 +0200

    VERDE: ordinamento di un array da tre elementi implementato

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -2,10 +2,14 @@ public class InsertionSort implements SorterState {
     @Override
     public int[] sort(int[] array) {
         if( array!=null && array.length>1 )
-            if(array[0]>array[1]) {
-                int temp = array[0];
-                array[0] = array[1];
-                array[1] = temp;
+            for (int i=1; i<array.length; i++){
+                int elementToOrder = array[i];
+                int j = i-1;
+                while ( j>=0 && array[j]>elementToOrder ){
+                    array[j+1] = array[j];
+                    j--;
+                }
+                array[j+1] = elementToOrder;
             }
         return array;
     }
