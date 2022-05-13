fa5cc2c249cbe53b063ec6bef5681d4dd0c1b7b1
mauro <freeMonkey-ls@protonmail.com>
2022-05-13 17:57:11 +0200

    VERDE: immutabilità dell'array dato in input al metodo sort implementata

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -1,17 +1,22 @@
+import java.util.Arrays;
+
 public class InsertionSort implements SorterState {
     @Override
     public int[] sort(int[] array) {
-        if( array!=null && array.length>1 )
-            for (int i=1; i<array.length; i++){
-                int elementToOrder = array[i];
-                int j = i-1;
-                while ( j>=0 && array[j]>elementToOrder ){
-                    array[j+1] = array[j];
+        int[] arrayToSort = array;
+        if( array!=null && array.length>1 ) {
+            arrayToSort = Arrays.copyOf(array, array.length);
+            for (int i = 1; i < arrayToSort.length; i++) {
+                int elementToOrder = arrayToSort[i];
+                int j = i - 1;
+                while (j >= 0 && arrayToSort[j] > elementToOrder) {
+                    arrayToSort[j + 1] = arrayToSort[j];
                     j--;
                 }
-                array[j+1] = elementToOrder;
+                arrayToSort[j + 1] = elementToOrder;
             }
-        return array;
+        }
+        return arrayToSort;
     }
 
     @Override
