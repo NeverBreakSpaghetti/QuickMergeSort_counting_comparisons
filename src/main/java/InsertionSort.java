7c9d9665f6fcb9d42701499439c10084ec731887
mauro <freeMonkey-ls@protonmail.com>
2022-05-13 16:56:52 +0200

    VERDE: ordinamento di un array da due elementi implementato

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -1,6 +1,12 @@
 public class InsertionSort implements SorterState {
     @Override
     public int[] sort(int[] array) {
+        if( array!=null && array.length>1 )
+            if(array[0]>array[1]) {
+                int temp = array[0];
+                array[0] = array[1];
+                array[1] = temp;
+            }
         return array;
     }
 
