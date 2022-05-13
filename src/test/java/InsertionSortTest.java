0cdac2a44ad9605b678b0ac99db6dcd0e1702142
mauro <freeMonkey-ls@protonmail.com>
2022-05-13 17:31:14 +0200

    REFACTORING: estratta inizializzazione oggetto InsertionSort

--- src/test/java/InsertionSortTest.java
+++ src/test/java/InsertionSortTest.java
@@ -5,46 +5,35 @@ import java.util.Arrays;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class InsertionSortTest {
+    SorterState insertionSort = new InsertionSort();
 
     @Test
     public void getKeysComparisonsNumberSorterStateTest(){
-        SorterState insertionSort = new InsertionSort();
-
         assertThat(insertionSort.getKeysComparisonsNumber()).isEqualTo(0);
     }
 
     @Test
     public void emptyArraySortTest(){
-        SorterState insertionSort = new InsertionSort();
-
         assertThat(Arrays.toString(insertionSort.sort(new int[]{}))).isEqualTo("[]");
     }
 
     @Test
     public void nullArraySortTest(){
-        SorterState insertionSort = new InsertionSort();
-
         assertThat(insertionSort.sort(null)).isEqualTo(null);
     }
 
     @Test
     public void oneElementArraySortTest(){
-        SorterState insertionSort = new InsertionSort();
-
         assertThat(Arrays.toString(insertionSort.sort(new int[]{1}))).isEqualTo("[1]");
     }
 
     @Test
     public void twoElementsArraySortTest(){
-        SorterState insertionSort = new InsertionSort();
-
         assertThat(Arrays.toString(insertionSort.sort(new int[]{2,1}))).isEqualTo("[1, 2]");
     }
 
     @Test
     public void threeElementsArraySortTest(){
-        SorterState insertionSort = new InsertionSort();
-
         assertThat(Arrays.toString(insertionSort.sort(new int[]{2,3,1}))).isEqualTo("[1, 2, 3]");
     }
 }
