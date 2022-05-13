7dee1c2d1cf3cf1df8d4b604935b34392f486ec9
mauro <freeMonkey-ls@protonmail.com>
2022-05-13 18:50:23 +0200

    VERDE: conteggio delle comparazione dell'ordinamento di un array da tre elementi implementato

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -13,11 +13,14 @@ public class InsertionSort implements SorterState {
             for (int i = 1; i < arrayToSort.length; i++) {
                 int elementToOrder = arrayToSort[i];
                 int j = i - 1;
+                keysComparisonsNumber++;
                 while (j >= 0 && arrayToSort[j] > elementToOrder) {
                     keysComparisonsNumber++;
                     arrayToSort[j + 1] = arrayToSort[j];
                     j--;
                 }
+                if(j<0)
+                    keysComparisonsNumber--;
                 arrayToSort[j + 1] = elementToOrder;
             }
         }
