a5463b24a03f60def27976a4bde316cccc72d54a
mauro <freeMonkey-ls@protonmail.com>
2022-05-16 15:48:14 +0200

    REFACTORING: estratta istanziazione dell'oggetto HeapSort dai test

--- src/test/java/HeapSortTest.java
+++ src/test/java/HeapSortTest.java
@@ -6,24 +6,26 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HeapSortTest {
+    HeapSort heapSort = new HeapSort();
     @Test
     public void getKeysComparisonsNumberSorterStateTest(){
-        SorterState heapSortSorterState = new HeapSort();
+        SorterState heapSortSorterState = heapSort;
         assertThat(heapSortSorterState.getKeysComparisonsNumber()).isEqualTo(0);
     }
 
     @Test
     public void setAndGetArrayToSortTest(){
-        HeapSort heapSort = new HeapSort();
         heapSort.setArrayToSort(new int[]{3,2});
+
         assertThat(Arrays.toString(heapSort.getArray())).isEqualTo("[3, 2]");
     }
 
     @Test
     public void nullArrayToHeapifyTest(){
-        HeapSort heapSort = new HeapSort();
         heapSort.setArrayToSort(null);
+        
         heapSort.heapify(0);
+
         assertThat(Arrays.toString(heapSort.getArray())).isEqualTo("null");
     }
 }
