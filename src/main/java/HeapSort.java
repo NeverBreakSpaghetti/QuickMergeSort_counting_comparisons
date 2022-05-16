2dfded79e856e07ca953ded45d6fe2850361e4e4
mauro <freeMonkey-ls@protonmail.com>
2022-05-16 15:57:17 +0200

    VERDE: heapify dato un array da due elementi implementato

--- src/main/java/HeapSort.java
+++ src/main/java/HeapSort.java
@@ -21,5 +21,15 @@ public class HeapSort implements SorterState {
     }
 
     public void heapify(int end) {
+        if( arrayToSort != null ) {
+            int nodePosition = end / 2 - 1;
+            int leftChildPosition = nodePosition + 1;
+
+            if (arrayToSort[nodePosition] <= arrayToSort[leftChildPosition]) {
+                int temp = arrayToSort[nodePosition];
+                arrayToSort[nodePosition] = arrayToSort[leftChildPosition];
+                arrayToSort[leftChildPosition] = temp;
+            }
+        }
     }
 }
